5a53affb113170ceb638bdae547753560034587e
minidebu <debudebo3@gmail.com>
2022-03-28 20:55:40 +0900

    ルートコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+
   def item_params
     params.require(:item).permit(:name, :info, :category_id, :status_id, :prefecture_id, :postage_by_id, :shipping_days_id, :price,
                                  :image).merge(user_id: current_user.id)
