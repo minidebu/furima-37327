afacd47a920867864425509fa399c739439a07bd
minidebu <debudebo3@gmail.com>
2022-04-04 03:38:24 +0900

    orderコントローラーの記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @buyer_buy_record = BuyerBuyRecord.new
+  end
+
+  def create
+    @buyer_buy_record = BuyerBuyRecord.new(buy_params)
+    if @buyer_buy_record.valid?
+      @buyer_buy_record.save
+      redirect_to root_path
+    else
+      render :index
+    end
 
   end
   
+  private
+  def buy_params
+    params(:BuyerBuyRecord).permit(:post_code ,:prefecture_id ,:municipality,:address,:building,:phone).merge(user_id:current_user.id,item_id:params[:item_id])
+
+  end
 end
