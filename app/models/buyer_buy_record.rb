09da1d4e6645208f70395e77d9f2ec42f48527b8
minidebu <debudebo3@gmail.com>
2022-04-06 03:10:11 +0900

    購入機能のバリデーション修正

--- app/models/buyer_buy_record.rb
+++ app/models/buyer_buy_record.rb
@@ -4,15 +4,16 @@ class BuyerBuyRecord
 
 
   with_options presence: true do
-    validates :post_code ,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_code 
+    validates :phone
     validates :municipality
     validates :address
     validates :user_id
     validates :item_id
   end
-  validates :phone ,numericality: {only_integer: true}, length: { in: 10..11 }
-  validates :prefecture_id ,numericality: {other_than:1}
-
+  validates :phone ,numericality: {only_integer: true}, length: { in: 10..11 },allow_blank: true
+  validates :prefecture_id ,numericality: {other_than:1},allow_blank: true
+  validates :post_code ,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"},allow_blank: true
   def save
  
     buy_record = BuyRecord.create(user_id:user_id,item_id:item_id)
