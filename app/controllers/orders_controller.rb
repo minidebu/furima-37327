df8e1376b652ea71de3ffb8dd5d307e6d4a2a673
minidebu <debudebo3@gmail.com>
2022-04-07 04:58:39 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,9 @@ class OrdersController < ApplicationController
   end
 
   def create
-
     @buyer_buy_record = BuyerBuyRecord.new(buy_params)
     if @buyer_buy_record.valid?
+      pay_item
       @buyer_buy_record.save
       redirect_to root_path
     else
@@ -22,10 +22,20 @@ class OrdersController < ApplicationController
   
   private
   def buy_params
-    params.require(:buyer_buy_record).permit(:post_code ,:prefecture_id ,:municipality,:address,:building,:phone).merge(user_id:current_user.id,item_id:params[:item_id])
+    params.require(:buyer_buy_record).permit(:post_code ,:prefecture_id ,:municipality,:address,:building,:phone).merge(user_id:current_user.id,item_id:params[:item_id]).merge(token:params[:token])
   end
   def move_to_index
     item = Item.find(params[:item_id])
     redirect_to root_path if BuyRecord.exists?(item_id:item.id) || item.user_id == current_user.id
+  end 
+  def pay_item
+    item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: item.price,
+      card: params[:token],
+      currency: 'jpy'
+    )
   end
+
 end
--- app/models/buyer_buy_record.rb
+++ app/models/buyer_buy_record.rb
@@ -1,7 +1,7 @@
 class BuyerBuyRecord
   include ActiveModel::Model
-  attr_accessor :post_code ,:prefecture_id ,:municipality,:address,:building,:phone,:user_id,:item_id
-
+  attr_accessor :post_code ,:prefecture_id ,:municipality,:address,:building,
+                :phone,:user_id,:item_id,:token
 
   with_options presence: true do
     validates :post_code 
@@ -10,17 +10,17 @@ class BuyerBuyRecord
     validates :address
     validates :user_id
     validates :item_id
+    validates :token
   end
   validates :phone ,numericality: {only_integer: true}, length: { in: 10..11 },allow_blank: true
   validates :prefecture_id ,numericality: {other_than:1},allow_blank: true
   validates :post_code ,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"},allow_blank: true
+
+
   def save
- 
     buy_record = BuyRecord.create(user_id:user_id,item_id:item_id)
     Buyer.create(post_code:post_code,prefecture_id:prefecture_id,municipality:municipality,address:address,building:building,phone:phone,buy_record_id:buy_record.id)
   end
-
-
 
 
 end
