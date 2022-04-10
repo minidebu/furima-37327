c180437fd744b6225e4c6163e0cd570fd19d19f4
minidebu <debudebo3@gmail.com>
2022-04-10 18:47:15 +0900

    migrationファイルの順序変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_03_162941) do
+ActiveRecord::Schema.define(version: 2022_04_04_154957) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
